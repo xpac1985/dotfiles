a07d49289628b7d2cfe5cf7960824fe5d3431755
xpac1985 <xpac1985@users.noreply.github.com>
2017-11-28 21:15:41 +0100

    included .gitconfig to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [ ! -f ~/.bashrc ]
 then
   ln -s dotfiles/bashrc ~/.bashrc
-  echo "bashrc linked"
+  echo ".bashrc linked"
 else
   echo ".bashrc already exists"
 fi
@@ -11,7 +11,7 @@ fi
 if [ ! -f ~/.nanorc ]
 then
   ln -s dotfiles/nanorc ~/.nanorc
-  echo "nanorc linked"
+  echo ".nanorc linked"
 else
   echo ".nanorc already exists"
 fi
@@ -19,7 +19,7 @@ fi
 if [ ! -f ~/.tmux.conf ]
 then
   ln -s dotfiles/tmux.conf ~/.tmux.conf
-  echo "tmux.conf linked"
+  echo ".tmux.conf linked"
 else
   echo ".tmux.conf already exists"
 fi
@@ -27,7 +27,15 @@ fi
 if [ ! -f ~/.profile ]
 then
   ln -s dotfiles/profile ~/.profile
-  echo "profile linked"
+  echo ".profile linked"
 else
   echo ".profile already exists"
+fi
+
+if [ ! -f ~/.gitconfig ]
+then
+  ln -s dotfiles/gitconfig ~/.gitconfig
+  echo ".gitconfig linked"
+else
+  echo ".gitconfig already exists"
 fi
